4e63cc368e32d25122b3ba612c7208450a689eb7
poomin <poominjaichoung@gmail.com>
2019-11-05 11:13:27 +0700

    cherry

--- controller/processController.php
+++ controller/processController.php
@@ -28,6 +28,7 @@ if(isset($result['id'])) {
 
     $sql = " WHERE main_id = $this_main_id ORDER BY sq ASC";
     $result = $MMPhase->selectSqlAll($sql);
+
     if(count($result)>0){
         $PHASES = $result;
         foreach ($PHASES as $key=>$item){
@@ -35,10 +36,13 @@ if(isset($result['id'])) {
             $count_pass = 0;
             $count_fail = 0;
             $sql= 'select phase.* from b2i_project as pro left join b2i_project_phase as phase on pro.id = phase.project_id where pro.main_id = '.$item['main_id'];
+            $sqlAll = $sql;
             if($item['sq'] == 1){
                 $sql = $sql. ' and ( phase.sq = '.$item['sq'] . ' or phase.sq IS NULL )';
+                $sqlAll = $sqlAll. ' and ( phase.sq = '.$item['sq'] . ' or phase.sq IS NULL )';
             }else{
                 $sql = $sql. ' and phase.sq = '.$item['sq'];
+                $sqlAll = $sqlAll. ' and phase.sq = '.(intval($item['sq'])-1) . ' and phase.phase_status ="PASS"';
             }
             $result = $MMain->sqlAll($sql);
             foreach ($result as $k=>$i){
@@ -49,7 +53,8 @@ if(isset($result['id'])) {
                     $count_fail = $count_fail+1;
                 }
             }
-            $PHASES[$key]['count_all'] = $count_all;
+            $result = $MMain->sqlAll($sqlAll);
+            $PHASES[$key]['count_all'] = count($result);
             $PHASES[$key]['count_pass'] = $count_pass;
             $PHASES[$key]['count_fail'] = $count_fail;
         }
